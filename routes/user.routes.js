7d929d615c23e4d6484ce6bbe0d85886324a1f17
Praveen Sharma <praying.sharma@gmail.com>
2025-08-19 15:34:53 +0530

    Signin Routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,18 @@ import { userTable } from "../models/index.js";
 import { eq } from "drizzle-orm";
 import { signupRequestBodySchema } from "../validation/request.validation.js";
 import { randomBytes, createHmac } from "crypto";
+import jwt from "jsonwebtoken";
+import { success } from "zod";
 const router = express.Router();
+
+async function getUserByEmail(email) {
+  const [user] = await db
+    .select()
+    .from(userTable)
+    .where(eq(userTable.email, email));
+
+  return user;
+}
 
 router.post("/signup", async (req, res) => {
   const validationResult = await signupRequestBodySchema.safeParse(req.body);
@@ -58,6 +69,46 @@ router.post("/signup", async (req, res) => {
     console.log(error);
     res.status(500).json({ success: false, message: error.message });
   }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Email and password is not found" });
+  }
+
+  try {
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please Try with correct email id" });
+    }
+
+    const hashedInputPassword = createHmac("sha256", user.salt)
+      .update(password)
+      .digest("hex");
+
+    if (hashedInputPassword !== user.password) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = await jwt.sign({ id: user.id }, process.env.SECREAT_KEY);
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: { userId: user.id, email: user.email, firstname: user.firstname },
+      token: token,
+    });
+  } catch (error) {}
 });
 
 export default router;
