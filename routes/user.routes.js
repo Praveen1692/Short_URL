82722e0fde6ef8fcda4253590920e9b25f51469f
Praveen Sharma <praying.sharma@gmail.com>
2025-08-19 13:28:00 +0530

    User Routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import {db} from "../db/index.js"
-import {userTable} from "../models/index.js"
+import { db } from "../db/index.js";
+import { userTable } from "../models/index.js";
+import { eq } from "drizzle-orm";
+
+import { salt, randomBytes, createHmac } from "crypto";
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
-  const { firstname, lastname, email, password } = req.body;
+  const { firstname, lastname, email, xword } = req.body;
 
   if (!firstname || !lastname || !email || !password) {
     return res
@@ -12,8 +15,44 @@ router.post("/signup", async (req, res) => {
       .json({ success: false, message: "All fields are mandetory" });
   }
   try {
+    const [existingUser] = await db
+      .select({
+        id: userTable.id,
+      })
+      .from(userTable)
+      .where(eq(userTable.email, email));
 
-    const existingUser=await db.
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: `Please try with different email , user already exit with the ${email} id `,
+      });
+    }
+
+    const salt = randomBytes(256).toString("hex");
+
+    const hashPassword = createHmac("sha256", salt)
+      .update(password)
+      .digest("hex");
+
+    const [user] = await db
+      .insert(userTable)
+      .values({
+        firstname,
+        lastname,
+        email,
+        salt,
+        password: hashPassword,
+      })
+      .returning({ id: userTable.id });
+
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "User Created Successfully",
+        data: { userId: user.id },
+      });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error.message });
